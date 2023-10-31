791bccb6b7eb91fcd329b8a630008331dd295430
cristianodc <cristianocoffy@gmail.com>
2023-10-31 11:14:08 -0300

    Criação do projeto

---
+++ Loja_Virtual_Ment/src/main/java/br/com/loja_v_ment/LojaVirtualMentApplication.java
@@ -0,0 +1,13 @@
+package br.com.loja_v_ment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualMentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualMentApplication.class, args);
+	}
+
+}
