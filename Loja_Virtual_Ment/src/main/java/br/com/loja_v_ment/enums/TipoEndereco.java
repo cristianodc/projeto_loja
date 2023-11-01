7e5a204473dfc3a7d7996b64d3857344f64dbd77
cristianodc <cristianocoffy@gmail.com>
2023-11-01 10:41:25 -0300

    Criado Endereco tipoEndereco e Pessoa

---
+++ Loja_Virtual_Ment/src/main/java/br/com/loja_v_ment/enums/TipoEndereco.java
@@ -0,0 +1,26 @@
+package br.com.loja_v_ment.enums;
+
+public enum TipoEndereco {
+	
+	
+	COBRANCA("Cobrança"),
+	ENTREGA("Entrega");
+	
+	private String descricao;
+	
+	
+	private TipoEndereco(String descricao) {
+	  this.descricao = descricao;
+	}
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+
+}
