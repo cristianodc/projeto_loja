118c966cd642460040af11cc5457d000377a821e
cristianodc <cristianocoffy@gmail.com>
2023-11-10 14:15:02 -0300

    AULA - Criando Banco e Classes com Spring Data JPA - Parte 6 10_11_23

---
+++ Loja_Virtual_Ment/src/main/java/br/com/loja_v_ment/model/AvaliacaoProduto.java
@@ -0,0 +1,102 @@
+package br.com.loja_v_ment.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "avaliacao_prod")
+@SequenceGenerator(name = "seq_avaliacao_prod", sequenceName = "seq_avaliacao_prod", allocationSize = 1, initialValue = 1)
+public class AvaliacaoProduto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_avaliacao_prod")
+	private Long id;
+		
+	private Integer nota;
+	
+	private String descricao;
+	@ManyToOne
+	@JoinColumn(name = "produto_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+	private 	Produto produto;
+
+	
+	@ManyToOne
+	@JoinColumn(name = "pessoa_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+	private 	Pessoa pessoa;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getNota() {
+		return nota;
+	}
+
+	public void setNota(Integer nota) {
+		this.nota = nota;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AvaliacaoProduto other = (AvaliacaoProduto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
